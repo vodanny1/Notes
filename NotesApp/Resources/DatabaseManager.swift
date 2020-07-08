aece4a97425c1a2406f4ec9665496767829bf3c4
Danny Vo <43733358+vodanny1@users.noreply.github.com>
2020-07-07 20:11:33 -0400

    set up database insertion

--- NotesApp/Resources/DatabaseManager.swift
+++ NotesApp/Resources/DatabaseManager.swift
@@ -8,3 +8,56 @@
 
 import Foundation
 import FirebaseDatabase
+
+final class DatabaseManager {
+    
+    // Singleton - creates one instance
+    static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+}
+
+// MARK: - Account Management
+
+extension DatabaseManager {
+    
+    public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "2", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value) { (snapshot) in
+            guard snapshot.value as? String != nil else {
+                completion(false)
+                return
+            }
+            completion(true)
+        }
+    }
+    
+    /// Inserts new user to database
+    public func insertUser(with user: NoteUser, completion: @escaping (Bool) -> Void) {
+        database.child(user.safeEmail).setValue([
+            "first_name": user.firstName,
+            "last_name": user.lastName
+            ], withCompletionBlock: { error, _ in
+                guard error == nil else {
+                    print("Failed to write to database")
+                    completion(false)
+                    return
+                }
+                completion(true)
+        })
+    }
+}
+
+struct NoteUser {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "2", with: "-")
+        return safeEmail
+    }
+}
